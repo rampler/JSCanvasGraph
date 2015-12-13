43a4a1f35ad512b9120c8cffb5d61d71479445b5
rampler <rampler@gmail.com>
2015-12-13 22:47:50 +0100

    Some functionality of left menu + new features of context menu

--- js/main.js
+++ js/main.js
@@ -6,14 +6,32 @@ var graphElem = document.getElementById('jsCanvasGraph');
 var graph = jsCanvasGraph.createGraph(graphElem);
 
 //Test
-var test1Node = new jsCanvasGraph.createNode(graph,1,50,50);
-var test2Node = new jsCanvasGraph.createNode(graph,2,300,50);
-var test3Node = new jsCanvasGraph.createNode(graph,3,550,50);
+var test1Node = new jsCanvasGraph.createNode(graph,50,50);
+var test2Node = new jsCanvasGraph.createNode(graph,300,50);
+var test3Node = new jsCanvasGraph.createNode(graph,550,50);
 
 var testEdge = new jsCanvasGraph.linkNodes(graph,test1Node,test2Node);
 var test2Edge = new jsCanvasGraph.linkNodes(graph,test2Node,test3Node);
 
 //Buttons actions
+$('#addBtn').click(function(){
+    jsCanvasGraph.createNode(graph, 0, 0);
+});
+
+$('#fitBtn').click(function(){
+    jsCanvasGraph.fitNodesToGrid(graph);
+});
+
+$('#loadBtn').click(function(){
+    jsCanvasGraph.loadGraph(graph,null);
+    //TODO odczyt
+});
+
+$('#saveBtn').click(function(){
+    var json = jsCanvasGraph.clearGraph(graph);
+    //TODO zapis
+});
+
 $('#clearBtn').click(function(){
     jsCanvasGraph.clearGraph(graph);
 });
